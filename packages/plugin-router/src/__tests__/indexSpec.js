323d36ff1aefa8c6e86bc3e70e9ea29e5828fcb1
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2020-06-04 23:49:54 +0200

    feat: 🎸 added plugin-router

---
+++ packages/plugin-router/src/__tests__/indexSpec.js
@@ -0,0 +1,130 @@
+import { createMerkurWidget } from '@merkur/core';
+import { routerPlugin, createRouter } from '../index';
+import { componentPlugin } from '@merkur/plugin-component';
+import { eventEmitterPlugin } from '@merkur/plugin-event-emitter';
+
+describe('createWidget method with router plugin', () => {
+  it('should create empty widget', async () => {
+    const widget = await createMerkurWidget({
+      $plugins: [componentPlugin, eventEmitterPlugin, routerPlugin],
+      name: 'my-widget',
+      version: '1.0.0',
+      props: {
+        param: 1,
+        pathName: '/my-route',
+      },
+      assets: [
+        {
+          type: 'script',
+          source: 'http://www.example.com/static/1.0.0/widget.js',
+        },
+      ],
+    });
+
+    expect(widget).toMatchInlineSnapshot(`
+      Object {
+        "$dependencies": Object {},
+        "$external": Object {},
+        "$in": Object {
+          "component": Object {
+            "isHydrated": false,
+            "isMounted": false,
+            "lifeCycle": Object {
+              "bootstrap": undefined,
+              "info": undefined,
+              "load": [Function],
+              "mount": undefined,
+              "unmount": undefined,
+              "update": undefined,
+            },
+          },
+          "eventEmitter": Object {
+            "event": Object {},
+          },
+          "router": Object {
+            "isRouteActivated": false,
+            "originalFunctions": Object {
+              "mount": [Function],
+              "unmount": [Function],
+              "update": [Function],
+            },
+            "route": null,
+          },
+        },
+        "$plugins": Array [
+          Object {
+            "create": [Function],
+            "setup": [Function],
+          },
+          Object {
+            "setup": [Function],
+          },
+          Object {
+            "create": [Function],
+            "setup": [Function],
+          },
+        ],
+        "assets": Array [
+          Object {
+            "source": "http://www.example.com/static/1.0.0/widget.js",
+            "type": "script",
+          },
+        ],
+        "bootstrap": [Function],
+        "create": [Function],
+        "emit": [Function],
+        "info": [Function],
+        "load": [Function],
+        "mount": [Function],
+        "name": "my-widget",
+        "off": [Function],
+        "on": [Function],
+        "props": Object {
+          "param": 1,
+          "pathName": "/my-route",
+        },
+        "router": Object {
+          "link": [Function],
+          "redirect": [Function],
+        },
+        "setProps": [Function],
+        "setState": [Function],
+        "setup": [Function],
+        "state": Object {},
+        "unmount": [Function],
+        "update": [Function],
+        "version": "1.0.0",
+      }
+    `);
+  });
+
+  describe('router plugin API', () => {
+    let widget = null;
+    const routes = [
+      {
+        path: '/my-route',
+        name: 'my-route',
+        action: () => 'My route',
+      },
+      { path: '/other-route', name: 'other-route', action: () => 'Page Two' },
+    ];
+
+    beforeEach(async () => {
+      widget = await createMerkurWidget({
+        name: 'my-widget',
+        version: '1.0.0',
+        $plugins: [componentPlugin, eventEmitterPlugin, routerPlugin],
+        props: {
+          pathName: '/my-route',
+        },
+      });
+
+      createRouter(widget, routes);
+    });
+
+    it('should be', async () => {
+      // widget.mount();
+      //
+    });
+  });
+});
---
+++ packages/plugin-router/src/index.js
@@ -0,0 +1,211 @@
+import { bindWidgetToFunctions } from '@merkur/core';
+
+import UniversalRouter from 'universal-router';
+import generateUrls from 'universal-router/generateUrls';
+
+const DEV = 'development';
+const ENV =
+  typeof process !== 'undefined' && process && process.env
+    ? process.env.NODE_ENV
+    : DEV;
+
+export function createRouter(widget, routes, options) {
+  widget.$dependencies.router = new UniversalRouter(routes, options);
+  widget.$dependencies.link = generateUrls(widget.$dependencies.router);
+}
+
+export const ROUTER_EVENTS = {
+  REDIRECT: '@mekur/plugin-router.redirect',
+};
+
+export function routerPlugin() {
+  return {
+    async setup(widget) {
+      widget = {
+        ...routerAPI(),
+        ...widget,
+      };
+
+      widget.$in.router = {
+        route: null,
+        isRouteActivated: false,
+        originalFunctions: {},
+      };
+
+      bindWidgetToFunctions(widget, widget.router);
+
+      return widget;
+    },
+    async create(widget) {
+      if (ENV === DEV) {
+        if (!widget.$in.component) {
+          throw new Error(
+            'You must install missing plugin: npm i @merkur/plugin-component'
+          );
+        }
+
+        if (!widget.$in.eventEmitter) {
+          throw new Error(
+            'You must install missing plugin: npm i @merkur/plugin-event-emitter'
+          );
+        }
+      }
+
+      widget.$in.component.lifeCycle.load = loadHook;
+      const { mount, unmount, update } = widget;
+
+      widget.$in.router.originalFunctions = {
+        mount,
+        unmount,
+        update,
+      };
+
+      widget.mount = mountHook;
+      widget.unmount = unmountHook;
+      widget.update = updateHook;
+
+      return widget;
+    },
+  };
+}
+
+function routerAPI() {
+  return {
+    router: {
+      redirect(widget, url) {
+        widget.emit(ROUTER_EVENTS.REDIRECT, { url });
+      },
+      link(widget, routeName, data = {}) {
+        return widget.$dependencies.link(routeName, data);
+      },
+    },
+  };
+}
+
+// hook Component
+async function loadHook(widget, ...rest) {
+  const plugin = widget.$in.router;
+
+  await tearDownRouterCycle(widget, ...rest);
+
+  await setupRouterCycle(widget, ...rest);
+
+  if (!isFunction(plugin.route.load)) {
+    throw new Error('The load method is mandatory.');
+  }
+
+  return plugin.route.load(widget, { route: plugin.route, args: rest });
+}
+
+// hook Component
+async function mountHook(widget, ...rest) {
+  const result = await widget.$in.router.originalFunctions.mount(
+    widget,
+    ...rest
+  );
+
+  const plugin = widget.$in.router;
+
+  if (!plugin.route) {
+    await setupRouterCycle(widget, ...rest);
+  }
+
+  if (
+    isFunction(plugin.route.activate) &&
+    isClient() &&
+    !plugin.isRouteActivated
+  ) {
+    plugin.isRouteActivated = true;
+    plugin.route.activate(widget, { route: plugin.route, args: rest });
+  }
+
+  return result;
+}
+
+// hook Component
+async function updateHook(widget, ...rest) {
+  const result = await widget.$in.router.originalFunctions.update(
+    widget,
+    ...rest
+  );
+
+  const plugin = widget.$in.router;
+
+  if (
+    isFunction(plugin.route.activate) &&
+    isClient() &&
+    !plugin.isRouteActivated
+  ) {
+    plugin.isRouteActivated = true;
+    plugin.route.activate(widget, { route: plugin.route, args: rest });
+  }
+
+  return result;
+}
+
+// hook Component
+async function unmountHook(widget, ...rest) {
+  const result = await widget.$in.router.originalFunctions.unmount(
+    widget,
+    ...rest
+  );
+
+  await tearDownRouterCycle(widget, ...rest);
+
+  return result;
+}
+
+async function resolveRoute(widget) {
+  if (ENV === DEV) {
+    if (!widget.props.pathname) {
+      throw new Error('The props pathname is not defined.');
+    }
+
+    if (!widget.$dependencies.router) {
+      throw new Error(
+        'You must add calling createRouter(widget, routes, options) to widget.bootstrap method.'
+      );
+    }
+  }
+
+  return widget.$dependencies.router.resolve({
+    pathname: widget.props.pathname,
+    widget,
+  });
+}
+
+async function setupRouterCycle(widget, ...rest) {
+  const route = await resolveRoute(widget);
+
+  if (isFunction(route.init)) {
+    await route.init(widget, { route, args: rest });
+  }
+
+  widget.$in.router.route = route;
+}
+
+async function tearDownRouterCycle(widget, ...rest) {
+  const plugin = widget.$in.router;
+
+  const { route, isRouteActivated } = plugin;
+
+  if (route) {
+    if (isFunction(route.deactivate) && isRouteActivated === true) {
+      await route.deactivate(widget, { route, args: rest });
+    }
+
+    if (isFunction(route.destroy)) {
+      await route.destroy(widget, { route, args: rest });
+    }
+  }
+
+  plugin.isRouteActivated = false;
+}
+
+function isFunction(value) {
+  return typeof value === 'function';
+}
+
+function isClient() {
+  return typeof window !== 'undefined';
+}
