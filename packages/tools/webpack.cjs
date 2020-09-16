e12581838273571be2c88d03c5061ed33857b0a2
Jan Šimeček <simecekjann@gmail.com>
2020-09-16 09:29:30 +0200

    fix: 🐛 Fixed babel-loader config to re-enable tree-shaking

--- packages/tools/webpack.cjs
+++ packages/tools/webpack.cjs
@@ -186,7 +186,7 @@ function applyES5Transformation(config, options = {}) {
   const babelPresetEnv = [
     '@babel/preset-env',
     {
-      modules: 'cjs',
+      modules: 'auto',
       useBuiltIns: 'usage',
       corejs: { version: 3, proposals: false },
     },
