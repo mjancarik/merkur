512ccc07c853dacb8048095667b68c46553ae78c
Jan Šimeček <simecekjann@gmail.com>
2020-10-12 09:06:13 +0200

    Revert "refactor: 💡 merkur-integration refactor"

--- packages/integration-react/src/__tests__/indexSpec.jsx
+++ packages/integration-react/src/__tests__/indexSpec.jsx
@@ -1,7 +1,7 @@
 import * as module from '../index';
 
-describe('module @merkur/integration-react', () => {
-  it('should have interface', () => {
+describe('module @merkur/integration-reacto', () => {
+  it('should has interface', () => {
     expect(module).toMatchInlineSnapshot(`
       Object {
         "MerkurComponent": [Function],
