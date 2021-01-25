7116f2c83019858bd93023a95dd89d63c73f4d41
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2021-01-25 18:27:29 +0100

    test: 💍 fix es-check test

--- packages/tool-storybook/rollup.config.js
+++ packages/tool-storybook/rollup.config.js
@@ -2,8 +2,19 @@ import {
   createRollupESConfig,
   createRollupES5Config,
 } from '../../createRollupConfig';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 
 let esConfig = createRollupESConfig();
 let es5Config = createRollupES5Config();
+
+const babelConfig = {
+  babelrc: false,
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  ],
+};
+
+esConfig.plugins.push(getBabelOutputPlugin(babelConfig));
 
 export default [esConfig, es5Config];
