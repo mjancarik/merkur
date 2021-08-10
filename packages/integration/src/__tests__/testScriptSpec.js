8f3d95b7cb03016f49c377dc2d187cd61d1245a2
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2021-08-10 22:06:29 +0200

    feat: 🎸 add isES11Supported method to testScript

--- packages/integration/src/__tests__/testScriptSpec.js
+++ packages/integration/src/__tests__/testScriptSpec.js
@@ -25,6 +25,30 @@ describe('Merkur component', () => {
     });
   });
 
+  describe('isES11Supported() function', () => {
+    beforeEach(() => {
+      jest.spyOn(testScript, 'test');
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should return true', () => {
+      const isES11Supported = testScript.isES11Supported();
+
+      expect(isES11Supported).toEqual(true);
+      expect(testScript.test).toHaveBeenCalledTimes(4);
+    });
+
+    it('should return true but not test again', () => {
+      const isES11Supported = testScript.isES11Supported();
+
+      expect(isES11Supported).toEqual(true);
+      expect(testScript.test).toHaveBeenCalledTimes(0);
+    });
+  });
+
   describe('test() function', () => {
     it('should return true', () => {
       expect(testScript.test('return typeof window !== "undefined"')).toEqual(
