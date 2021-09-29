6cb65765d985f112e3116869a036bbc55c6a7736
Jan Šimeček <simecekjann@gmail.com>
2021-09-29 18:58:20 +0200

    feat: 🎸 Added eslint plugin import and eslint react-hooks plugi

--- packages/tools/eslint.config.js
+++ packages/tools/eslint.config.js
@@ -1,25 +1,76 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
   rules: {
     'prettier/prettier': [
       'error',
       {
         singleQuote: true,
-        jsxBracketSameLine: true,
+        jsxSingleQuote: true,
       },
     ],
-
     'no-console': [
       'error',
       {
         allow: ['warn', 'error'],
       },
     ],
-
     'react/prop-types': 0,
     'react/wrap-multilines': 0,
     'react/no-deprecated': 0,
+    'import/first': ['error'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        json: 'never',
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: '{preact|react|svelte}{/**,**}',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@merkur{/**,**}',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: '#/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '*.{less,json}',
+            group: 'object',
+            patternOptions: { matchBase: true },
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['#/', '@merkur'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
   plugins: ['prettier', 'jest', 'react', 'jasmine'],
   settings: {
@@ -28,6 +79,10 @@ module.exports = {
     react: {
       version: '16',
     },
+    'import/ignore': [
+      'node_modules',
+      '\\.(coffee|scss|css|less|hbs|svg|json)$',
+    ],
   },
   parserOptions: {
     sourceType: 'module',
