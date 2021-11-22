fb71cab2ba6a7b3ace8f384f9ddff719ea875606
Vojtěch Šimko <SimkoVojta@gmail.com>
2021-11-22 12:35:28 +0100

    test: 💍 added tests for GenericError

--- packages/plugin-error/src/GenericError.js
+++ packages/plugin-error/src/GenericError.js
@@ -3,7 +3,7 @@ import ExtensibleError from './ExtensibleError';
 export default class GenericError extends ExtensibleError {
   constructor(message, params) {
     super(message);
-    const { status = 404, ...otherParams } = params;
+    const { status = 500, ...otherParams } = params;
 
     this.name = 'Error';
     this.status = status;
