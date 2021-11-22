a85ec68e452376ed4f055f75ed979bb2ae863335
Vojtěch Šimko <SimkoVojta@gmail.com>
2021-11-22 12:35:28 +0100

    test: 💍 added tests for GenericError

---
+++ packages/plugin-error/src/__tests__/GenericErrorSpec.js
@@ -0,0 +1,18 @@
+import GenericError from '../GenericError';
+
+describe('GenericError', () => {
+  it('should be able to create error instance', () => {
+    const error = new GenericError('Error', {
+      status: 500,
+      reason: 'api_error',
+    });
+
+    expect(error).toBeInstanceOf(GenericError);
+    expect(error.status).toBe(500);
+    expect(error.params).toEqual(
+      expect.objectContaining({
+        reason: 'api_error',
+      })
+    );
+  });
+});
