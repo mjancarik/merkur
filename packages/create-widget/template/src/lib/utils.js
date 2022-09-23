a5a965ab1db6d1c3c9892414d7cb3270b5a07b5c
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2022-09-23 22:30:58 +0200

    fix: 🐛 mapViews is async only for mounting methods
    Always async mapViews method cause problem with updating and unmounting
    widget in external application.

--- packages/create-widget/template/src/lib/utils.js
+++ packages/create-widget/template/src/lib/utils.js
@@ -1,25 +1,38 @@
-async function mapViews(widget, factoryFn, callback) {
-  const { View, slot = {} } = await factoryFn(widget);
-  const { containerSelector } = widget;
+function mapViews(widget, factoryFn, callback) {
+  if (widget.$external.resolvedViews) {
+    return mapResolvedViews(widget, callback);
+  }
 
-  // Add container selectors defined on widget instance after creation
-  Object.keys(widget.slot).forEach((slotName) => {
-    slot[slotName].isSlot = true;
-    slot[slotName].containerSelector = widget.slot[slotName].containerSelector;
-  });
-
-  return [
-    { View, containerSelector, isSlot: false },
-    ...Object.values(slot),
-  ].map(({ View, containerSelector, isSlot }) => {
-    callback({
-      View,
-      isSlot,
-      containerSelector,
-      container:
-        containerSelector && document?.querySelector(containerSelector),
+  return factoryFn(widget).then(({ View, slot = {} }) => {
+    const { containerSelector } = widget;
+    // Add container selectors defined on widget instance after creation
+    Object.keys(widget.slot).forEach((slotName) => {
+      slot[slotName].isSlot = true;
+      slot[slotName].containerSelector =
+        widget.slot[slotName].containerSelector;
     });
+
+    widget.$external.resolvedViews = [
+      { View, containerSelector, isSlot: false },
+      ...Object.values(slot),
+    ];
+
+    return mapResolvedViews(widget, callback);
   });
+}
+
+function mapResolvedViews(widget, callback) {
+  return widget.$external.resolvedViews.map(
+    ({ View, containerSelector, isSlot }) => {
+      callback({
+        View,
+        isSlot,
+        containerSelector,
+        container:
+          containerSelector && document?.querySelector(containerSelector),
+      });
+    }
+  );
 }
 
 export { mapViews };
