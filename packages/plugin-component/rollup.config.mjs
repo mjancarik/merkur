52e5551f26930ba9eea044a6a95e13ba5fcfa583
Jan Šimeček <simecekjann@gmail.com>
2024-04-04 10:40:19 +0200

    feat: 🎸 Added mapViews helper to plugin-component

--- packages/plugin-component/rollup.config.mjs
+++ packages/plugin-component/rollup.config.mjs
@@ -2,10 +2,21 @@ import {
   createRollupESConfig,
   createRollupES9Config,
   createRollupUMDConfig,
+  createRollupTypescriptConfig,
 } from '../../createRollupConfig.mjs';
 
 let esConfig = createRollupESConfig();
 let es9Config = createRollupES9Config();
 let umdConfig = createRollupUMDConfig();
 
-export default [esConfig, es9Config, umdConfig];
+export default [
+  esConfig,
+  es9Config,
+  umdConfig,
+  {
+    ...createRollupTypescriptConfig({
+      input: './src/helpers.ts',
+      dir: './lib',
+    }),
+  },
+];
