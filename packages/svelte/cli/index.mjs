6706746c12ec5d8652def6c9735e8132bbcead5d
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2024-04-09 08:12:29 +0200

    feat: 🎸 add svelte esbuild configuration

--- packages/svelte/cli/index.mjs
+++ packages/svelte/cli/index.mjs
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'node:url';
 
-//import sveltePlugin from 'esbuild-svelte';
+import sveltePlugin from 'esbuild-svelte';
 
 export default function ({ emitter, EMITTER_EVENTS, cliConfig }) {
   emitter.on(
@@ -23,28 +23,28 @@ export default function ({ emitter, EMITTER_EVENTS, cliConfig }) {
     },
   );
 
-  // emitter.on(
-  //   EMITTER_EVENTS.TASK_BUILD,
-  //   function defaultBuild({ build, config }) {
-  //     build = {
-  //       ...build,
-  //       mainFields: ['svelte', 'browser', 'module', 'main'],
-  //       conditions: ['svelte', 'browser'],
-  //       plugins: [
-  //         ...build.plugins,
-  //         sveltePlugin({
-  //           compilerOptions: {
-  //             dev: cliConfig.watch,
-  //             generate: config.isServer ? 'ssr' : 'dom',
-  //             hydratable: true,
-  //             css: !config.isServer,
-  //           },
-  //           cache: cliConfig.watch,
-  //         }),
-  //       ],
-  //     };
+  emitter.on(
+    EMITTER_EVENTS.TASK_BUILD,
+    function defaultBuild({ build, config }) {
+      build = {
+        ...build,
+        mainFields: ['svelte', 'browser', 'module', 'main'],
+        conditions: ['svelte', 'browser'],
+        plugins: [
+          ...build.plugins,
+          sveltePlugin({
+            compilerOptions: {
+              dev: cliConfig.watch,
+              generate: config.isServer ? 'ssr' : 'dom',
+              hydratable: true,
+              css: !config.isServer,
+            },
+            cache: cliConfig.watch,
+          }),
+        ],
+      };
 
-  //     return build;
-  //   },
-  // );
+      return build;
+    },
+  );
 }
