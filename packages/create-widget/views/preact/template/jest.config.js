0f471411d4856894a162ddb5f5692d33302a0c26
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2024-08-25 22:02:44 +0200

    feat: 🎸 first integration of testing-library
    ✅ Closes: #63

--- packages/create-widget/views/preact/template/jest.config.js
+++ packages/create-widget/views/preact/template/jest.config.js
@@ -1,9 +1,11 @@
 const config = require('@merkur/tools/jest.config.js');
 
 config.setupFilesAfterEnv = ['./jest.setup.js'];
-config.snapshotSerializers = ['enzyme-to-json/serializer'];
 config.transform = {
-  '^.+\\.[t|j]sx?$': 'esbuild-jest',
+  '^.+\\.[t|j]sx?$': [
+    'es-jest',
+    { jsx: 'automatic', jsxImportSource: 'preact' },
+  ],
 };
 
 module.exports = {
