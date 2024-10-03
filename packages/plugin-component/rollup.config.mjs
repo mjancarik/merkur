04f082cfd79e33b496223f427a8c20c892bf432f
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2024-10-03 16:39:12 +0200

    chore: fix build

--- packages/plugin-component/rollup.config.mjs
+++ packages/plugin-component/rollup.config.mjs
@@ -8,14 +8,18 @@ let esConfig = createRollupESConfig();
 let es9Config = createRollupES9Config();
 let umdConfig = createRollupUMDConfig();
 
+let helpersConfig = createRollupESConfig();
+let helpersES9Config = createRollupES9Config();
+let umdHelpersConfig = createRollupUMDConfig();
+helpersConfig.input = './src/helpers.js';
+helpersES9Config.input = './src/helpers.js';
+umdHelpersConfig.input = './src/helpers.js';
+
 export default [
   esConfig,
   es9Config,
   umdConfig,
-  createRollupESConfig({
-    input: './src/helpers.js',
-  }),
-  createRollupES9Config({
-    input: './src/helpers.js',
-  }),
+  helpersConfig,
+  helpersES9Config,
+  umdHelpersConfig,
 ];
