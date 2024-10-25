a345b065cfc9c81a6b1bf317930afa4995b2dac2
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2024-10-25 10:58:59 +0200

    fix: 🐛 create folder structure for copied file

--- packages/cli/src/commands/custom.mjs
+++ packages/cli/src/commands/custom.mjs
@@ -11,6 +11,7 @@ import { handleExit } from '../handleExit.mjs';
 
 export const CUSTOM_PART = {
   PLAYGROUND_BODY: 'playground:body',
+  PLAYGROUND_FOOTER: 'playground:footer',
 };
 
 export async function custom({ args, command }) {
@@ -26,22 +27,37 @@ export async function custom({ args, command }) {
   const logger = createLogger('Custom command:', cliConfig);
 
   await handleExit({ context });
+
+  let file = null;
+
   switch (args.part) {
     case CUSTOM_PART.PLAYGROUND_BODY: {
-      const file = 'body.ejs';
-      const src = path.resolve(
-        `${merkurConfig.playground.templateFolder}/${file}`,
-      );
-      const dest = path.resolve(
-        `${merkurConfig.playground.serverTemplateFolder}/${file}`,
-      );
-
-      logger.debug(`Copy file from ${src} to ${dest}`);
-
-      await fs.copyFile(src, dest);
-
-      logger.info(`You can customize ${chalk.green(dest)} file.`);
+      file = 'body.ejs';
       break;
     }
+    case CUSTOM_PART.PLAYGROUND_FOOTER: {
+      file = 'footer.ejs';
+      break;
+    }
+  }
+
+  if (file) {
+    const src = path.resolve(
+      `${merkurConfig.playground.templateFolder}/${file}`,
+    );
+    const dest = path.resolve(
+      `${merkurConfig.playground.serverTemplateFolder}/${file}`,
+    );
+    const dir = path.dirname(dest);
+
+    logger.debug(`Create directory ${dir}`);
+    await fs.mkdir(dir, { recursive: true });
+
+    logger.debug(`Copy file from ${src} to ${dest}`);
+    await fs.copyFile(src, dest);
+
+    logger.info(`You can customize ${chalk.green(dest)} file.`);
+  } else {
+    logger.warn(`You define unknown part ${args.part}`);
   }
 }
