71ef8d6b7e0bf162ab42729ae00e908589c2a508
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2025-03-14 14:22:28 +0100

    fix: 🐛 devServer propagete status code from error

--- packages/cli/src/devServer.mjs
+++ packages/cli/src/devServer.mjs
@@ -168,8 +168,9 @@ export async function runDevServer({ context, merkurConfig, cliConfig }) {
           return next(error);
         }
 
-        logger.error(error);
-        res.status(500).json({
+        logger.error(error)
+        // TODO send html page with preview of error for better DX
+        res.status(error?.status ?? 500).json({
           error: {
             message: `Something is wrong with the @merkur/cli/devServer: ${error.message}`,
             stack: error.stack,
