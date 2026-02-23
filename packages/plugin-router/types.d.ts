e5b28eb73f06118f1fd63dcabcf00a31446faeea
Franková, Anna <anna.frankova@firma.seznam.cz>
2026-02-23 09:49:45 +0100

    feat: 🎸 Add basic types to plugin-router
    Add some minimal typing to plugin router via a *.d.ts file.

---
+++ packages/plugin-router/types.d.ts
@@ -0,0 +1,40 @@
+import { WidgetPlugin } from '@merkur/core';
+import { ViewType } from '@merkur/plugin-component';
+
+export interface Route {
+  name: string;
+  path: string;
+  action: (params: RouteParams) => Pick<
+    WidgetDefinition,
+    'mount' | 'update' | 'unmount'
+  > & {
+    PageView: ViewType;
+    load: () => Promise<void>;
+  };
+}
+
+declare module '@merkur/core' {
+  interface WidgetPartial {
+    router: {
+      redirect: (
+        widget: WidgetPartial,
+        url: string,
+        data?: Record<string, any>,
+      ) => void;
+      link: (
+        widget: WidgetPartial,
+        routeName: string,
+        data?: Record<string, any>,
+      ) => string;
+      getCurrentRoute: (widget: WidgetPartial) => Route;
+    };
+  }
+}
+
+declare module '@merkur/plugin-component' {
+  interface WidgetProps {
+    pathname: string;
+  }
+}
+
+declare function routerPlugin(): WidgetPlugin;
