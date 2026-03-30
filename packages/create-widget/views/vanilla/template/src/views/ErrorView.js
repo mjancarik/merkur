5d79474671e4d69af2d7464b1d5c9d97753b5e03
Jiří Fencl <jiri.fencl@firma.seznam.cz>
2026-03-30 14:46:34 +0200

    Revert ErrorView args escaping

--- packages/create-widget/views/vanilla/template/src/views/ErrorView.js
+++ packages/create-widget/views/vanilla/template/src/views/ErrorView.js
@@ -1,17 +1,9 @@
-function escHtml(s) {
-  return String(s)
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;');
-}
-
 export default function ErrorView(widget) {
   return `
     <div class='merkur__error'>
-      <h1>Status: ${escHtml(widget.error.status)}</h1>
-      <h2>Message: ${escHtml(widget.error.message)}</h2>
-      <pre>${escHtml(widget.error.stack)}</pre>
+      <h1>Status: ${widget.error.status}</h1>
+      <h2>Message: ${widget.error.message}</h2>
+      <pre>${widget.error.stack}</pre>
     </div>
   `;
 }
